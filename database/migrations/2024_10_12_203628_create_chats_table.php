18fc66838ea5f50c331768e6f69fccfe4f2caf4c
DkPhobos <kucherya.alexander@gmail.com>
2024-10-13 15:41:16 +0300

    migration, controllers created. Env&db options changed to InnoDB

---
+++ database/migrations/2024_10_12_203628_create_chats_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 127);
+            $table->timestamps();
+        });
+
+        
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('chat_id')->constrained();
+            $table->timestamps();
+            $table->text('body')->nullable();
+            $table->text('deleted_body')->nullable();
+            $table->index(['chat_id', 'created_at']);
+        });
+
+        Schema::create('chat_user', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('chat_id')->constrained();
+            $table->timestamps();
+
+            $table->primary(['user_id', 'chat_id']);
+            $table->index(['user_id', 'updated_at']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chat_user');
+        Schema::dropIfExists('messages');
+        Schema::dropIfExists('chats'); 
+    }
+};

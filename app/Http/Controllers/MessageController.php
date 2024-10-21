961b70b9ebd3a8c61fcae0f3461f019e73d52b0f
Oleksandr Kucheria <kucherya.alexander@gmail.com>
2024-10-21 19:37:11 +0500

    Chat v 3.0 with sockets created

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\MessageType;
+use App\Events\Chat\MessageEvent;
 use App\Http\Resources\MessageResource;
 use App\Models\Chat;
 use App\Models\Chat_User;
@@ -41,6 +43,8 @@ class MessageController extends Controller
             ];
         }
 
+        broadcast(new MessageEvent($message, MessageType::MessageSent))->toOthers();
+
         return new MessageResource($message);
     }
 
@@ -53,6 +57,8 @@ class MessageController extends Controller
         $message->body = $validated['body'];
         $message->save();
 
+        broadcast(new MessageEvent($message, MessageType::MessageUpdated))->toOthers();
+
         return new MessageResource($message);
     }
 
@@ -62,6 +68,8 @@ class MessageController extends Controller
         $message->deleted_body = null;
 
         $message->save();
+
+        broadcast(new MessageEvent($message, MessageType::MessageRestored))->toOthers();
 
         return new MessageResource($message);
     }
@@ -73,6 +81,17 @@ class MessageController extends Controller
 
         $message->save();
 
+        broadcast(new MessageEvent($message, MessageType::MessageDestroyed))->toOthers();
+
         return new MessageResource($message);
+    }
+
+    public function updateLastSeen(Chat $chat, Message $message) 
+    {
+        return Chat_User::where([
+            'chat_id' => $chat->id,
+            'user_id' => Auth::id(),
+        ])->where('last_seen_message_id', '<', $message->id)
+          ->update(['last_seen_message_id' => $message->id]);
     }
 }
--- resources/js/Pages/Chat/ChatMain.vue
+++ resources/js/Pages/Chat/ChatMain.vue
@@ -9,6 +9,7 @@ import ChatHello from '@/Components/Chat/ChatHello.vue';
 import { router } from '@inertiajs/vue3'
 import axios from 'axios';
 import { ref, watch, onMounted } from 'vue';
+import '../../echo';
 
 axios.interceptors.response.use(
     (response) => response,
@@ -16,11 +17,9 @@ axios.interceptors.response.use(
         if (error.response) {
             if ([401, 419, 500].includes(error.response.status)) {
                 //router.reload({ preserveScroll: true, preserveState: true });
-                //some annoying things >.<
                 window.location.reload();
             }
         }
-        handleError(error);
         return Promise.reject(error);
     }
 );
@@ -42,25 +41,34 @@ const activeChat = ref({});
 const form = useForm({
     body: null
 });
+//Save message for restoring if u decide to cancel Editing
 const savedMessage = ref(null);
 
 const page = usePage();
 
 onMounted(() => {
     if (activeChatId.value) {
+        //We should load and init chat list before chat data loaded, but we can do it in async way
         Promise.all(
             [initChatList(), requestChatDataById(activeChatId.value)])
-            .then(res => {
-                setChatListData(res[0]);
-                setChatData(activeChatId.value, res[1]);
+            .then(([chatListRes, chatDataRes]) => {
+                setChatListData(chatListRes);
+                setChatData(activeChatId.value, chatDataRes);
+
+                listenToChatCreatedEvent(page.props.auth.user.id);
+                chatListRes.forEach(chat => listenToChatMessageEvent(chat.id));
             })
             .catch((error) => {
                 handleError(error);
             });
     } else {
+        //no chat selected from the start
         initChatList()
             .then(chatListRes => {
                 setChatListData(chatListRes);
+
+                listenToChatCreatedEvent(page.props.auth.user.id);
+                chatListRes.forEach(chat => listenToChatMessageEvent(chat.id));
             })
             .catch((error) => {
                 handleError(error);
@@ -68,18 +76,52 @@ onMounted(() => {
     }
 });
 
+function listenToChatCreatedEvent(userId) {
+    return Echo.private(`user.${userId}`)
+        .listen('.chat_created', (chat) => {
+            addChatToChatList(chat);
+        }).error((error) => {
+            handleError(error);
+        })
+}
+
+function listenToChatMessageEvent(chatId) {
+    return Echo.private(`chat.${chatId}`)
+        .listen('.message_sent', (message) => {
+            if (activeChatId.value === chatId) {
+                //You're already in chat, so you'll see the message and we should update it
+                addMessageToChat(chatId, message, true);
+                updateLastSeenMessage(chatId, message.id);
+            } else {
+                addMessageToChat(chatId, message);
+            }
+        })
+        .listen('.message_updated', (message) => {
+            editMessageInChat(chatId, message);
+        })
+        .listen('.message_restored', (message) => {
+            restoreMessageInChat(chatId, message);
+        }).
+        listen('.message_destroyed', (message) => {
+            destroyMessageInChat(chatId, message);
+        });
+}
+
 watch(
     () => page.url,
     (newURL, oldURL) => {
         const chatIdMatch = newURL.match(/\/chats\/(\d+)/);
         if (chatIdMatch) {
+            //we've opened new chat
             activeChatId.value = parseInt(chatIdMatch[1]);
             getChatDataById(activeChatId.value);
             activeChat.value = getChatById(activeChatId.value);
         } else {
+            //no chat selected
             activeChatId.value = null;
             activeChat.value = null;
         }
+        //clear form data
         form.body = null;
         savedMessage.value = null;
     }
@@ -98,21 +140,24 @@ async function requestChatDataById(chatId) {
 }
 
 function setChatListData(data) {
+    //cache results
     chats.value = data;
     activeChat.value = getChatById(activeChatId.value);
 }
 
 function setChatData(chatId, data) {
+    //cache results
     messages.value[chatId] = data.messages;
     chatUsers.value[chatId] = data.users;
     const messageCount = data.messages.length;
     if (messageCount) {
-        UpdateChatListLastMessage(getChatIndexById(chatId), data.messages[messageCount - 1], true);
+        changeChatListLastMessage(getChatById(chatId), data.messages[messageCount - 1], true);
     }
 }
 
 //Get Messages and Users in Chat via request
 function getChatDataById(chatId) {
+    //if messages of current chat wasnt loaded and cached
     if (!messages.value[chatId]) {
         requestChatDataById(chatId)
             .then(res => {
@@ -121,17 +166,29 @@ function getChatDataById(chatId) {
             .catch((error) => {
                 handleError(error);
             });
+    } else {
+        //mb some unreaded message if data was loaded, cached and then again received
+        const messageCount = messages.value[chatId].length;
+        const lastMessageId = messageCount && messages.value[chatId][messageCount - 1].id;
+        const index = getChatIndexById(chatId);
+        if (lastMessageId && lastMessageId !== chats.value[index].last_seen_message_id) {
+            chats.value[index].last_seen_message_id = lastMessageId;
+            updateLastSeenMessage(chatId, lastMessageId);
+        }
     }
 }
 
-function UpdateChatListLastMessage(index, message, seen = true) {
-    chats.value[index].last_message = {
-        body: message.body,
-        id: message.id
-    };
-    chats.value[index].updated_at = message.updated_at;
+function changeChatListLastMessage(chat, message, seen = false) {
+    Object.assign(chat, {
+        last_message: {
+            body: message.body,
+            id: message.id
+        },
+        updated_at: message.updated_at
+    })
+
     if (seen)
-        chats.value[index].last_seen_message_id = message.id;
+        chat.last_seen_message_id = message.id;
 }
 
 function onEditClick(messageId, index) {
@@ -152,27 +209,25 @@ function getChatById(id) {
     return chats.value.find(chat => chat.id === id);
 }
 
-function pushChat(index, message = null) {
-    chats.value.unshift(chats.value.splice(index, 1)[0]);
-    if (message) {
-        UpdateChatListLastMessage(0, message, true);
-    }
+function getMessageIndexById(chatId, messageId) {
+    const chatMessages = messages.value[chatId];
+    return chatMessages && chatMessages.findIndex(message => message.id === messageId);
+}
+
+function getMessageById(chatId, messageId) {
+    const chatMessages = messages.value[chatId];
+    return chatMessages && chatMessages.find(message => message.id === messageId);
+}
+
+function pushChat(chatId) {
+    const chat = chats.value.splice(getChatIndexById(chatId), 1)[0];
+    chats.value.unshift(chat);
+    return chat;
 }
 
 function onEditCancel() {
     form.body = savedMessage.value.body;
     savedMessage.value = null;
-}
-
-function onEditSubmit(messageId, index) {
-    axios.put(route('chats.messages.update', [activeChat.value.id, messageId]), form).then((res) => {
-        messages.value[activeChatId.value][index]['body'] = res.data.data.body;
-        messages.value[activeChatId.value][index]['updated_at'] = res.data.data.updated_at;
-        savedMessage.value = null;
-        form.body = null;
-    }).catch((error) => {
-        handleError(error);
-    });
 }
 
 function onSendMessage() {
@@ -183,38 +238,87 @@ function onSendMessage() {
 
     //create new message
     axios.post(route('chats.messages.store', activeChat.value.id), form).then((res) => {
-        const index = getChatIndexById(activeChat.value.id);
-        pushChat(index, res.data.data);
-
-        form.body = null;
-        res.data.data.user = {
-            id: page.props.auth.user.id,
-            name: page.props.auth.user.name
-        }
-        messages.value[activeChatId.value].push(res.data.data);
+        addMessageToChat(activeChat.value.id, res.data.data, true, true);
     }).catch((error) => {
         handleError(error);
     });
 };
 
+function addMessageToChat(chatId, message, messageSeen = false, authUserIsOwner = false) {
+    //authUser submited message
+    if (authUserIsOwner) {
+        message.user = {
+            id: page.props.auth.user.id,
+            name: page.props.auth.user.name
+        }
+        form.body = null;
+    }
+
+    //no data in cache
+    if (messages.value[chatId])
+        messages.value[chatId].push(message);
+
+    const chat = pushChat(chatId);
+    changeChatListLastMessage(chat, message, messageSeen);
+}
+
+function onEditSubmit(messageId, messageIndex) {
+    axios.put(route('chats.messages.update', [activeChat.value.id, messageId]), form).then((res) => {
+        editMessageInChat(activeChatId.value, res.data.data, messageIndex);
+        savedMessage.value = null;
+        form.body = null;
+    }).catch((error) => {
+        handleError(error);
+    });
+}
+
+function editMessageInChat(chatId, messageData) {
+    //we got update event, but mb we have not loaded this message, let try to check it
+    const message = updateMessageBodyInChat(chatId, messageData);
+
+    if (message) {
+        const chat = getChatById(chatId);
+        if (message.id === chat.last_message.id)
+            changeChatListLastMessage(chat, message, true);
+    }
+}
+
 function onDeleteClick(messageId, index) {
     if (window.confirm('Are you sure u want to delete?')) {
         axios.delete(route('chats.messages.destroy', [activeChat.value.id, messageId])).then((res) => {
-            messages.value[activeChatId.value][index]['body'] = res.data.data.body;
-            messages.value[activeChatId.value][index]['updated_at'] = res.data.data.updated_at;
+            destroyMessageInChat(activeChat.value.id, res.data.data);
         }).catch((error) => {
             handleError(error);
         });
     }
 }
 
+function destroyMessageInChat(chatId, messageData) {
+    return updateMessageBodyInChat(chatId, messageData);
+}
+
 function onRestoreClick(messageId, index) {
     axios.patch(route('chats.messages.restore', [activeChat.value.id, messageId])).then((res) => {
-        messages.value[activeChatId.value][index]['body'] = res.data.data.body;
-        messages.value[activeChatId.value][index]['updated_at'] = res.data.data.updated_at;
+        restoreMessageInChat(activeChat.value.id, res.data.data);
     }).catch((error) => {
         handleError(error);
     });
+}
+
+function restoreMessageInChat(chatId, messageData) {
+    return updateMessageBodyInChat(chatId, messageData);
+}
+
+function updateMessageBodyInChat(chatId, { id: messageId, body, updated_at }) {
+    const message = getMessageById(chatId, messageId)
+    if (message) {
+        Object.assign(message, {
+            body,
+            updated_at
+        })
+    }
+
+    return message;
 }
 
 function openChat(userId, callBack) {
@@ -222,9 +326,22 @@ function openChat(userId, callBack) {
         callBack();
         //chat created
         if (res.data.name) {
-            chats.value.unshift(res.data);
+            addChatToChatList(res.data);
         }
         router.visit(route('chats.show', res.data.id), { preserveScroll: true, preserveState: true })
+    }).catch((error) => {
+        handleError(error);
+    });
+}
+
+function addChatToChatList(chat) {
+    chats.value.unshift(chat);
+    listenToChatMessageEvent(chat.id);
+}
+
+function updateLastSeenMessage(chatIid, messageId) {
+    axios.patch(route('chats.messages.update.last_seen', [chatIid, messageId])).then(res => {
+        return res.id;
     }).catch((error) => {
         handleError(error);
     });
